504ac757da33ee35efff497e0d34c2bdaa1dbd44
yukkeechang <yukkeechang@gmail.com>
2021-05-15 01:27:55 -0400

    start UI on event page

--- src/components/meetings/event.jsx
+++ src/components/meetings/event.jsx
@@ -1,36 +1,21 @@
 import { React, useState } from 'react';
-// import Loading from './my-loading-component';
-import { Row, Col, Space, Image, Button } from 'antd';
-
-import { AiOutlineCalendar, AiOutlineEnvironment, AiOutlineUsergroupAdd, AiOutlineArrowRight } from 'react-icons/ai';
+import { Row, Col, Space,  Image,  Button } from 'antd';
+import { AiOutlineCalendar, AiOutlineEnvironment, AiOutlineUsergroupAdd, AiOutlinePlus, AiOutlineEdit } from 'react-icons/ai';
 import eventsService from '../../services/events.service';
-
-// const EventData = {
-//   userID: 1,
-//   name: 'Bi-Weekly Clean Up at Prospect Park',
-//   desc:
-//     'Lorem ipsum Messenger bag chartreuse craft beer, affogato tacos fashion axe palo santo kinfolk meditation austin skateboard green juice. Sriracha mustache ',
-//   locationName: 'Prospect Park',
-//   location: '123 Smith Street, Brooklyn, NY 10434',
-//   img: 'https://patersontimes.com/wp-content/uploads/2015/04/paterson-students-cleaning-great-falls.jpg',
-//   date: 'Tuesday, May 4th, 2021',
-//   time: '5:30-8:30pm',
-//   type: 'Park',
-//   amount: '8',
-//   attending: [
-//     { name: 'John Kim', userID: 2 },
-//     { name: 'Riley Adams', userID: 3 },
-//     { name: 'Julio Carlos', userID: 4 },
-//   ],
-//  };
+import ConvertDate from '../../Shared/Functions';
+import Divider from '../../Shared/Components';
+import '../../Shared/shared.less';
+import '../homepage/home.less';
 
 const getEvent = async (userID) => {
   const a = await eventsService.getSingleEvent(userID);
   return a;
 };
 
+
 export default function SingleEvent(props) {
   const [event, setEvent] = useState('');
+  // const { currentUser } = props;
     
   if (event === '') {
     getEvent(props.match.params.id).then((response) => {
@@ -38,82 +23,79 @@ export default function SingleEvent(props) {
     }); // the [1] is showing only that single event
   }
 
-  // const { currentUser } = props;
-if (event==null) 
-{
-  return <h1 style={{ color: 'Green', fontSize: 72 }}>Oops Event does not exist...</h1>
-}
+  if (event==null)  {
+      return <h1 style={{ color: 'Green', fontSize: 72 }}>Oops Event does not exist...</h1>
+    }
+  
+  const EditButton = () => {
+    return (
+      <a href='/myMeetUps/create' alt='' type='submit'>
+          <div className='round-button-lg' >
+            <Col>
+              <Space>
+                <div>Edit This Event</div>
+                <AiOutlineEdit size={24} />
+              </Space>
+            </Col>
+          </div>
+        </a>
+    )
+  }
 
   if (event !== '') {
     return (
       <div>
-        <a href='/myMeetUps/create' alt='' type='submit'>
-          <Button
-            shape='round'
-            style={{ height: 60, width: 240, backgroundColor: '#3EFFD1', borderRadius: 60, borderColor: '#3EFFD1' }}
-          >
-            <Col>
-              <Space>
-                Edit This Event
-                <AiOutlineArrowRight size={20} />
-              </Space>
+        <div style={{margin: '5%', padding: '5%', backgroundColor: 'white'}}>
+          <Row>
+            <Col span={12}>
+              <Image src={event.img} style={{borderTopLeftRadius: 10, height: '210px'}}/>
             </Col>
-          </Button>
-        </a>
-        
-                <div
+            <Col span={12} style={{borderTopRightRadius: 10, backgroundColor: '#F3F2F2', padding: '5%', height: '210px'}}>
+              <Row>
+                <AiOutlineCalendar color='#208970' size={24} style={{marginTop: '1.5%', marginRight: '1%'}}/>
+                <div className="banner-subheader">Someday, {ConvertDate(event.date)}</div>
+              </Row>
+              <Row> 
+                <AiOutlineEnvironment color='#208970' size={24}/>
+                <div>{event.location}</div>
+              </Row>
+              <Col span={12} offset={4}>
+                <Button
+                  shape='round'
                   style={{
-                    backgroundColor: '#208970',
-                    paddingTop: '80px',
-                    paddingBottom: '100px',
-                    marginBottom: '50px',
-                    marginTop: '50px',
+                    height: 40,
+                    width: 200,
+                    backgroundColor: '#AEFFCF',
+                    borderRadius: 30,
+                    borderColor: '#AEFFCF',
                   }}
                 >
-                  <Row>
-                    <Col span={12} offset={4}>
-                      <div style={{ height: 1, backgroundColor: '#3EFFD1', width: '70%' }} />
-                      <br />
-                      <h1 style={{ color: 'white', fontSize: 24 }}>{event.name}</h1>
-                      <Row>
-                        <Space>
-                          <AiOutlineCalendar color='#3EFFD1' size={24} />
-                          <Row gutter={24}>
-                            <Col>
-                              <h4 style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>{event.date}</h4>
-                            </Col>
-                            <Col>
-                              <h4 style={{ color: 'white', fontSize: 18, fontWeight: 'bold' }}>{event.time}</h4>
-                            </Col>
-                          </Row>
-                        </Space>
-                      </Row>
-                      <Space>
-                        <AiOutlineEnvironment color='#3EFFD1' size={24} />
-                        <Row gutter={24}>
-                          <Col>
-                            {' '}
-                            <h4 style={{ color: 'white' }}>Location:</h4>{' '}
-                          </Col>
-                          <Col>
-                            {' '}
-                            <h4 style={{ color: 'white' }}>{event.location}</h4>{' '}
-                          </Col>
-                        </Row>
-                      </Space>
-                      <Row>
-                        <AiOutlineUsergroupAdd color='#3EFFD1' size={24} />
-                        <h4 style={{ color: 'white' }}>{`${event.amount} people attending`}</h4>
-                      </Row>
-                    </Col>
-                    <Col span={6} style={{ paddingTop: '3%' }}>
-                      <Image src={event.img} style={{ height: '100%', width: '100%' }} />
-                    </Col>
-                    
-                  </Row>
-                </div>
-              );
+              <Col>
+                <Space>
+                  Join Event 
+                  <AiOutlinePlus color='#208970' size={20} />
+                </Space>
+              </Col>
+            </Button>
+           </Col>
+           
+          </Col>
+          
+          <Col style={{paddingLeft: '2%', paddingRight: '2%'}}>
+            <div className="banner-subheader">{event.name}</div>
+            <div>{event.description} Description of event! Join us for a community bi-weekly cleanup here at Prospect Park. Gloves and trashbags are provided. Bring a friend or 2!</div>
             
+            <Divider height={1} color='#C4C4C4'/>
+            <Row>
+              <AiOutlineUsergroupAdd/>
+              <div>{`${event.amount} attending`}</div>
+            </Row>
+            
+          </Col>
+         
+          </Row>
+        </div>
+        <EditButton/>
       </div>
     );
   }
