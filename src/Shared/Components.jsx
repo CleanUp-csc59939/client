10a0c08d68d71d580fd3f8ca19058ac5c31e6b98
shahzeb <shahzebrizvi16@gmail.com>
2021-05-17 18:25:26 -0400

    Fixed picture sizing

--- src/Shared/Components.jsx
+++ src/Shared/Components.jsx
@@ -19,16 +19,39 @@ export const Divider = (props) => {
 
 export const EventCarousel = (props) => {
   const { titleText, events, slidesToShow } = props;
+  
   return (
     <div>
       <h2 className='big-title v-title-pad horizontal-pad'>{titleText}</h2>
+
+      {slidesToShow === 1 ?
       <Carousel slidesToShow={slidesToShow} className={slidesToShow === 1 ? 'xtra-horizontal-pad' : 'horizontal-pad'}>
         {Object.keys(events).map((index) => {
           return (
             <Link to={`/event/${events[index].id}`} key={index}>
               <Row>
                 <Card
-                  cover={<img src={events[index].img[0]} alt='event' style={{ height: '180px' }} />}
+                  cover={<img src={events[index].img[0]} alt='event' style={{ height: '400px' }} />}
+                  style={{ width: '60%' }}
+                >
+                  <h1>{events[index].name}</h1>
+                  <p className='accent'>{ConvertDate(events[index].date)}</p>
+                </Card>
+              </Row>
+            </Link>
+          );
+        })}
+      </Carousel>
+
+      :
+
+      <Carousel slidesToShow={slidesToShow} className={slidesToShow === 1 ? 'xtra-horizontal-pad' : 'horizontal-pad'}>
+        {Object.keys(events).map((index) => {
+          return (
+            <Link to={`/event/${events[index].id}`} key={index}>
+              <Row>
+                <Card
+                  cover={<img src={events[index].img[0]} alt='event' style={{ height: '300px' }} />}
                   style={{ width: '90%' }}
                 >
                   <h1>{events[index].name}</h1>
@@ -39,6 +62,7 @@ export const EventCarousel = (props) => {
           );
         })}
       </Carousel>
+      }
     </div>
   );
 };
